64647892abf5c88e1a1ecaa784eabde139139cf0
Michael Abebe <notthismichael@gmail.com>
2025-09-24 10:59:15 +0300

    gradle wrapper is needed

---
+++ services/ambalay_mono/src/main/java/com/ambalay/maps/mono/AmbalayMono.java
@@ -0,0 +1,11 @@
+package com.ambalay.maps.mono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbalayMono {
+    public static void main(String[] args) {
+        SpringApplication.run(AmbalayMono.class, args);
+    }
+}
