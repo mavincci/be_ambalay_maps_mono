cd995226e9ff43a8f8ba913501aa82c9dafb4390
Michael Abebe <notthismichael@gmail.com>
2025-10-01 11:48:50 +0300

    removed migration for now so it can not be a blocker

--- services/ambalay_mono/src/main/resources/db/rollback/R0001__Drop_users_table.sql
+++
@@ -1,5 +0,0 @@
--- Manual rollback for V0001__Create_users_table.sql
--- Run manually if needed: psql -d db_pg_ambalay_maps_mono -f R0001__Drop_users_table.sql
-
-DROP TABLE IF EXISTS mono.users CASCADE;
-DROP EXTENSION IF EXISTS "uuid-ossp";
